2e4059fc08f17390381bbb8f6394e27724c575e0
Ignacio Paradisi <ignacioparadisi@icloud.com>
2026-08-27 13:03:16 -0700

    Fix bugs

--- generate-pdf.js
+++ generate-pdf.js
@@ -27,6 +27,8 @@ const generatePDF = async (htmlPath, outputPath) => {
     const pdfOptions = {
         path: outputPath,
         format: 'A4',
+        printBackground: true,
+        margin: { top: 0, right: 0, bottom: 0, left: 0 },
     };
 
     await page.pdf(pdfOptions);
